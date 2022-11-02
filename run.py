6e3326b5e171ba9077cca59a67ad6f22d666e407
Maurizio <maurizio.loffredo@outlook.com>
2022-11-02 21:49:43 +0000

    Feat: added validation function to check for exactly 6 values in provided data

--- run.py
+++ run.py
@@ -15,21 +15,22 @@ SCOPE = [
     ]
 
 # The JSON file with the credentials generated through GCP (Google Cloud Platform), 'creds.json', has been imported here.
-# CREDS is assigned by passing 'creds.json' as a parameter of the 'from_service_account_file' method of the 'Credentials' class:
+# CREDS is assigned by passing 'creds.json' as a parameter of the 'from_service_account_file()' method of the 'Credentials' class:
 
 CREDS = Credentials.from_service_account_file('creds.json')
 
-# SCOPED_CREDS is assigned by passing the SCOPE variable as a parameter of the 'with_scopes' method of the CREDS object:
+# SCOPED_CREDS is assigned by passing the SCOPE variable as a parameter of the 'with_scopes()' method of the CREDS object:
 
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 
-# GSPREAD_CLIENT is assigned by passing the SCOPED_CREDS variable as a parameter of the 'gspread.authorize' method:
+# GSPREAD_CLIENT is assigned by passing the SCOPED_CREDS variable as a parameter of the 'gspread.authorize()' method:
 
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
-# SHEET provides access to the 'love_sandwiches' spreadsheet and is assigned by passing the file name as a parameter of the 'open' method of the GSPREAD_CLIENT object:
+# SHEET provides access to the 'love_sandwiches' spreadsheet and is assigned by passing the file name as a parameter of the 'open()' method of the GSPREAD_CLIENT object:
 
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
 
 # Function to collect sales data from users:
 
@@ -41,7 +42,35 @@ def get_sales_data():
     print('Data should be six numbers, separated by commas.')
     print('Example: 10, 20, 30, 40, 50, 60\n')
 
+    # The user is prompted with a call to action:
+
     data_string = input('Enter data here: ')
-    print(f'The data provided is {data_string}')
+
+    # The 'split()' method returns the broken up values as a list, removing the specified punctuation (comma) used to enter data:
+
+    sales_data = data_string.split(',')
+
+    # The validation function is called, by passing it the list containing the sales data:
+
+    validate_data(sales_data)
+
+
+# Function validating user data entries; it accepts a parameter representing the sales data list:
+
+def validate_data(values):
+    """
+    It tries to convert the provided string values into integers and raises ValueError if:
+    - there are not exactly six values (length of the provided list not equal to 6);
+    - the strings cannot be converted into integers.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(f'Exactly 6 values are required; {len(values)} entered instead')
+
+    # The ValueError class contains the details of the error triggered in the try-block.
+    # The 'as' keyword allows for the assignement of the ValueError object to the 'e' variable, which is standard Python shorthand for 'error':
+    except ValueError as e:
+        print(f'Invalid data: {e}; please, try again.\n')
+
 
 get_sales_data()
